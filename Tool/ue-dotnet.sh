10d2f667a2e229fd108ed9b317ded968779113dd
Isamu Mogi <isamu@leafytree.jp>
2026-06-05 04:11:36 +0900

    engine association

--- Tool/ue-dotnet.sh
+++ Tool/ue-dotnet.sh
@@ -4,4 +4,5 @@ set -eu
 
 cd "$(dirname "$0")"
 
-echo "Not implemented yet"
+engine_association=$(python3 -c "import json; import pathlib; print(json.loads(pathlib.Path('UnrealMirror.uproject').read_text())['EngineAssociation'])")
+echo "Not implemented: Reading Unreal Engine root path from registry on Linux/MacOS. EngineAssociation: $engine_association"
