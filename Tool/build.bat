926adff95e1612e00acd32231d7b026a8d3db690
Isamu Mogi <isamu@leafytree.jp>
2026-06-05 14:16:02 +0900

    batch

--- Tool/build.bat
+++ Tool/build.bat
@@ -4,4 +4,4 @@ cd /d "%~dp0"
 
 call .\ue-dotnet.bat tool restore
 call .\ue-dotnet.bat tool run pwsh -- format.ps1
-call .\ue-dotnet.bat tool run pwsh -- uat-build-cook-run.ps1 $*
+call .\ue-dotnet.bat tool run pwsh -- uat-build-cook-run.ps1 %*
